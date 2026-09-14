5abb1b6832e4da52b0bfd5133a68b2cf17b2baab
davidemilizia <davidemilizia@gmail.com>
2026-09-14 13:17:04 +0200

    Fix formatting and indentation in app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ if uploaded_file:
         # NUOVE TIPOLOGIE
         # -----------------------
 
-        nuovi_omaggi = [
+                nuovi_omaggi = [
             gift
             for gift in omaggi_trovati
             if gift not in config
